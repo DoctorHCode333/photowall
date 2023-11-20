5fab1f6f5d5aba75eb4e6a7c49dcedfee180e24b
DoctorHCode333 <harshvardhanbawake@gmail.com>
2023-11-20 18:56:45 +0530

    state_management_update

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,7 +3,17 @@ import React,{Component} from "react";
 class Photo extends Component{
     render(){
         const post = this.props.post;
-        return <figure></figure>
+        return <figure className="figure">
+                <img className="photo" src={post.imageLink} alt={post.description}/>
+                <figcaption><p>{post.description}</p></figcaption>
+                <div className="button-container">
+                    <button className="remove-button" onClick = {()=> {
+                            this.props.onRemovePhoto(post);
+                        }}> 
+                        Remove 
+                    </button>
+                </div>
+            </figure>
     }  
 }
 
--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -5,7 +5,9 @@ import Photo from "./Photo";
 class Photowall extends Component{
     render(){
         return <div className = "photo-grid">
-              {this.props.posts.map((post,index) => <Photo key = {index} post = {post}/>)}
+            {
+            this.props.posts.map((post,index) => <Photo key = {index} post = {post} onRemovePhoto={this.props.onRemovePhoto}/> )
+            }
         </div>
     }
 }
