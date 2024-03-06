0d31528e63a3089fa63b7eb36abfb5cf8fe18eb6
DoctorHCode333 <harshvardhanbawake@gmail.com>
2024-03-06 18:33:38 +0530

    hashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './Components/App';
 import './Styles/style.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from './redux/reducer'
 import {Provider} from 'react-redux'
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux'
 const store = configureStore({reducer: rootReducer});
 
 const root = createRoot(document.getElementById('root'))
-root.render(<Provider store={store}><Router><App/></Router></Provider>);                                                                          
+root.render(<Provider store={store}><HashRouter><App/></HashRouter></Provider>);                                                                          
 
 
 
